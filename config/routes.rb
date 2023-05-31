a6ebf79d5899ee2e16e8231ccbb37747eb7adbf0
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-05-31 08:28:03 +0000

    顧客側サインアップ機能まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,39 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    
+  # 顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_scope :customer do
+    post '/customers/sign_up', to: 'public/registrations#create', as: 'custom_registration'
+  end
+
+  root to: "public/homes#top"
+  get '/about' => 'public/homes#about'
+  
+  scope module: 'customers' do
+    resources :items, only: [:show, :index]
+   end
+  
+  namespace :public do
+  end
+  
+  
+  
+  
+   # 管理者用
+   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+   
+   namespace :admin do
+    root :to => 'admin/homes#top'
+    # resources :customers, only: [:index, :show, :edit, :update]
+    # resources :items, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+    # resources :orders, only: [:index, :show, :update]
+    # resources :order_items, only: [:update]
+    # delete 'items'=> 'items#destroy'
+  end
 end
