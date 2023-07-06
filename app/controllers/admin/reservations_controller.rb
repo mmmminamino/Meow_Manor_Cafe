d8630c01d6e2206d6573305600514897299900cf
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-07-06 07:53:31 +0000

    サインアップ画面調整

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -2,27 +2,22 @@ class Admin::ReservationsController < ApplicationController
     before_action :authenticate_admin!
     
     def index
-        @search=Reservation.ransack(params[:q])
+        @search=Reservation.includes(:customer).ransack(params[:q])
         @reservations=@search.result.page(params[:page]).per(20)
     end
     
     def show
         @reservation=Reservation.find(params[:id])
-        @reservation.customer=Customer.find(params[:id])
+        @customer = @reservation.customer
     end
     
-    # def update
-    #     @reservation=Reservation.find(params[:id])
-    #     reservation_status=reservation.reservation_status
-    #     reservation.update(reservation_params)
-    #         if reservation.reservation_status=="入金確認"
-    #             reservation_items.update_all(making_status: "製作中")
-    #         end
-    #         redirect_to admins_reservation_path(reservation.id)
-    # end
+    def update
+        @reservation=Reservation.find(params[:id])
+        @customer = @reservation.customer
+    end
     
     private
         def reservation_params
-            params.require(:reservation).permit(:reservation_status)
+            params.require(:reservation).permit(:customer_id, :date_and_time, :number_of_people, :payment_methods, :status)
         end
 end
