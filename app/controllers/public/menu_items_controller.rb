92160cfa0395725ec445953c688ffd7eaf1a2ea6
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-07-07 11:15:08 +0000

    顧客カフェメニュー画面調整

--- app/controllers/public/menu_items_controller.rb
+++ app/controllers/public/menu_items_controller.rb
@@ -1,4 +1,13 @@
 class Public::MenuItemsController < ApplicationController
+  before_action :authenticate_customer!, only: [:index]
+  
+  
   def index
+    @menu_items=MenuItem.all
   end
+  
+  private
+    def menu_item_params
+        params.require(:menu_item).permit(:name, :price_without_tax)  
+    end
 end
