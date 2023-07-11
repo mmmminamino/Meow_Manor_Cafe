612cc76562d0e66c96242bb36a5b2922b9536661
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-07-11 03:38:46 +0000

    プリコンパイル調整

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,22 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
-module.exports = environment
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
+    $: 'jquery',
+    jQuery: 'jquery',
     Popper: 'popper.js'
   })
 )
+
+module.exports = environment
